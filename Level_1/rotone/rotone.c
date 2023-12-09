9825c16bf65a3e57f0be97754e41728088748eb1
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-12-09 16:44:54 +0100

    1337

---
+++ Level_1/rotone/rotone.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	if (ac != 2)
+	{
+		ft_putchar('\n');
+		return (0);
+	}
+	i = 0;
+	while (av[1][i])
+	{
+		if ('a' <= av[1][i] && av[1][i] <= 'z')
+			ft_putchar((av[1][i] - 'a' + 1) % 26 + 'a');
+		else if ('A' <= av[1][i] && av[1][i] <= 'Z')
+			ft_putchar((av[1][i] - 'A' + 1) % 26 + 'A');
+		else
+			ft_putchar(av[1][i]);
+		i++;
+	}
+	ft_putchar('\n');
+}
