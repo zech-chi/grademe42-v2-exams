c89b4e6967ca91f1c62d17806a095decd1a71cbb
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-12-13 20:21:28 +0100

    1337

--- Level_2/reverse_bits/reverse_bits.c
+++
@@ -1,9 +0,0 @@
-unsigned char	reverse_bits(unsigned char octet)
-{
-	int	shifted_left;
-	int	shifted_right;
-
-	shifted_left = octet << 4;
-	shifted_right = octet >> 4;
-	return (shifted_left | shifted_right);
-}
