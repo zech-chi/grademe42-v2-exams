2b2be58af9927bbe6d701f055f4ad85adfc52518
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-12-15 16:48:30 +0100

    1337

---
+++ Level_2/union/union.c
@@ -0,0 +1,45 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_search(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	if (ac != 3)
+	{
+		ft_putchar('\n');
+		return (0);
+	}
+	i = -1;
+	while (av[1][++i])
+	{
+		if (ft_search(av[1], av[1][i]) == i)
+			ft_putchar(av[1][i]);
+	}
+	i = -1;
+	while (av[2][++i])
+	{
+		if (ft_search(av[1], av[2][i]) == -1 && ft_search(av[2], av[2][i]) == i)
+			ft_putchar(av[2][i]);
+	}
+	ft_putchar('\n');
+	return (0);
+}
