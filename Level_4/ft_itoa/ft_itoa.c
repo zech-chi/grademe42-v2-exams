353393199ca2951c8fce3511dbea226a113f2e4c
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-12-16 00:28:27 +0100

    1337

---
+++ Level_4/ft_itoa/ft_itoa.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+
+int	ft_nbrlen(int nbr)
+{
+	int	len;
+
+	if (nbr == -2147483648)
+		return (11);
+	if (nbr == 0)
+		return (1);
+	len = 1 * (nbr < 0);
+	if (nbr < 0)
+		nbr *= -1;
+	while (nbr)
+	{
+		len++;
+		nbr /= 10;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int nbr)
+{
+	char	*res;
+	int	len;
+
+	len = ft_nbrlen(nbr);
+	res = (char *)malloc(sizeof(char) * (len + 1));
+	if (!res)
+		return (NULL);
+	if (nbr == 0)
+		return (res[0] = '0', res[1] = '\0', res);
+	if (nbr == -2147483648)
+	{
+		res[0] = '-';
+		res[1] = '2';
+		nbr = 147483648;
+	}
+	else if (nbr < 0)
+	{
+		res[0] = '-';
+		nbr *= -1;
+	}
+	res[len--] = '\0';
+	while (nbr)
+	{
+		res[len--] = '0' + nbr % 10;
+		nbr /= 10;
+	}
+	return (res);
+}
