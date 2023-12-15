f9c315123793bb49d7b2336c8f6fa25921455b0a
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-12-16 00:40:37 +0100

    1337

---
+++ Level_3/print_hex/print_hex.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+unsigned int	ft_uatoi(char *str)
+{
+	unsigned int	res;
+	int	i;
+
+	res = 0;
+	i = 0;
+	while (str[i] == ' ')
+		i++;
+	while ('0' <= str[i] && str[i] <= '9')
+		res = res * 10 + str[i++] - '0';
+	return (res);
+}
+
+void	ft_print_hex(unsigned int n)
+{
+	if (n < 16)
+		ft_putchar("0123456789abcdef"[n]);
+	else
+	{
+		ft_print_hex(n / 16);
+		ft_print_hex(n % 16);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		ft_putchar('\n');
+		return (0);
+	}
+	ft_print_hex(ft_uatoi(av[1]));
+	ft_putchar('\n');
+	return (0);
+}
