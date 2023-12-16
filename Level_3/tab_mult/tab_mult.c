80e6e3ce105fdc7074c3b4c02d9316817a6f9522
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-12-17 00:22:55 +0100

    1337

--- Level_3/tab_mult/tab_mult.c
+++ Level_3/tab_mult/tab_mult.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   alpha_mirror.c                                     :+:      :+:    :+:   */
+/*   tab_mult.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/16 23:56:09 by zech-chi          #+#    #+#             */
-/*   Updated: 2023/12/17 00:08:21 by zech-chi         ###   ########.fr       */
+/*   Created: 2023/12/17 00:10:30 by zech-chi          #+#    #+#             */
+/*   Updated: 2023/12/17 00:20:41 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,49 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+int	ft_atoi(char *str)
+{
+	int	res;
+	int	i;
+
+	i = 0;
+	while (str[i] == ' ')
+		i++;
+	res = 0;
+	while ('0' <= str[i] && str[i] <= '9')
+		res = res * 10 + str[i++] - '0';
+	return (res);
+}
+
+void	ft_putnbr(unsigned int u)
+{
+	if (u < 10)
+		ft_putchar('0' + u);
+	else
+	{
+		ft_putnbr(u / 10);
+		ft_putnbr(u % 10);
+	}
+}
+
 int	main(int ac, char **av)
 {
-	int	i;
+	int	n;
 
 	if (ac != 2)
 	{
 		ft_putchar('\n');
 		return (0);
 	}
-	i = -1;
-	while (av[1][++i])
+	n = ft_atoi(av[1]);
+	for (int i = 1; i < 10; i++)
 	{
-		if ('a' <= av[1][i] && av[1][i] <= 'z')
-			ft_putchar('z' - (av[1][i] - 'a'));
-		else if ('A' <= av[1][i] && av[1][i] <= 'Z')
-			ft_putchar('Z' - (av[1][i] - 'A'));
-		else
-			ft_putchar(av[1][i]);
+		ft_putnbr(i);
+		write(1, " x ", 3);
+		ft_putnbr(n);
+		write(1, " = ", 3);
+		ft_putnbr(n * i);
+		write(1, "\n", 1);
 	}
-	ft_putchar('\n');
 	return (0);
 }
