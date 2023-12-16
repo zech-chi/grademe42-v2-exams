13428b5f759265f1dfb33690132a520ae5a8cb05
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-12-16 17:28:02 +0100

    1337

--- Level_2/swap_bits/swap_bits.c
+++ Level_2/swap_bits/swap_bits.c
@@ -1,7 +1,19 @@
-unsigned char	reverse_bits(unsigned char octet)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   swap_bits.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 17:23:25 by zech-chi          #+#    #+#             */
+/*   Updated: 2023/12/16 17:23:26 by zech-chi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned char	swap_bits(unsigned char octet)
 {
-	int	shifted_left;
-	int	shifted_right;
+	unsigned char	shifted_left;
+	unsigned char	shifted_right;
 
 	shifted_left = octet << 4;
 	shifted_right = octet >> 4;
