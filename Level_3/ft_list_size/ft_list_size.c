e8946637f31da0d3b226d55922279e865343a30d
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-12-16 01:53:00 +0100

    1337

---
+++ Level_3/ft_list_size/ft_list_size.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_size.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 01:48:30 by zech-chi          #+#    #+#             */
+/*   Updated: 2023/12/16 01:49:44 by zech-chi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+int	ft_list_size(t_list *begin_list)
+{
+	int	len;
+
+	len = 0;
+	while (begin_list)
+	{
+		len++;
+		begin_list = begin_list->next;
+	}
+	return (len);
+}
