ab1ce00deeb4a95e81cf408ceaf2eaceb451e0f8
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-12-17 03:00:28 +0100

    1337

---
+++ Level_3/rstr_capitalizer/rstr_capitalizer.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rstr_capitalizer.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/17 02:47:11 by zech-chi          #+#    #+#             */
+/*   Updated: 2023/12/17 03:00:06 by zech-chi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_isSpace(char c)
+{
+	return (c == ' ' || c == '\t');
+}
+
+int	ft_isLower(char c)
+{
+	return ('a' <= c && c <= 'z');
+}
+
+int	ft_isUpper(char c)
+{
+	return ('A' <= c && c <= 'Z');
+}
+
+int	main(int ac, char **av)
+{
+	int	r;
+	int	c;
+
+	if (ac == 1)
+	{
+		ft_putchar('\n');
+		return (0);
+	}
+	r = 1;
+	while (r < ac)
+	{
+		c = 0;
+		while (av[r][c])
+		{
+			if (ft_isLower(av[r][c]))
+			{
+				if (!av[r][c + 1] || ft_isSpace(av[r][c + 1]))
+					ft_putchar(av[r][c] - 32);
+				else
+					ft_putchar(av[r][c]);
+			}
+			else if (ft_isUpper(av[r][c]))
+			{
+				if (!av[r][c + 1] || ft_isSpace(av[r][c + 1]))
+					ft_putchar(av[r][c]);
+				else
+					ft_putchar(av[r][c] + 32);
+			}
+			else
+				ft_putchar(av[r][c]);
+			c++;
+		}
+		ft_putchar('\n');
+		r++;
+	}
+	return (0);
+}
